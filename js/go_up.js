66724080813be61d1588a23540dd142e5c6af4ac
MarcoPaoletta <84252721+MarcoPaoletta@users.noreply.github.com>
2022-02-17 13:03:18 -0300

    Boton para volver arriba

---
+++ js/go_up.js
@@ -0,0 +1,6 @@
+const go_up_button = document.querySelector("#go-up-button"); // obtain the button
+
+go_up_button.addEventListener("click", function () { // if press the button
+    window.scrollTo(0, 0); // go to the top of the page
+});
+

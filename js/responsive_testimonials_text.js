8f6d5296a1765e83e7dee1b738d5b3d84ffdf131
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-10-24 15:34:43 -0300

    Responsive testimonial-text name

--- js/responsive_testimonials_text.js
+++ js/responsive_testimonials_text.js
@@ -1,12 +1,10 @@
 function adjustTextElements() {
-    if (window.matchMedia("(max-width: 1210px)").matches) {
-        var firstText = document.querySelector(".testimonials-text");
-        var firstTextHeight = firstText.clientHeight - 30;
+    var firstText = document.querySelector(".testimonials-text");
+    var firstTextHeight = firstText.clientHeight;
 
-        var testimonialsTextElements = document.querySelectorAll(".testimonials-text");
-        for (var i = 0; i < testimonialsTextElements.length; i++) {
-            testimonialsTextElements[i].style.minHeight = firstTextHeight + "px";
-        }
+    var testimonialsTextElements = document.querySelectorAll(".testimonials-text");
+    for (var i = 0; i < testimonialsTextElements.length; i++) {
+        testimonialsTextElements[i].style.minHeight = firstTextHeight + "px";
     }
 }
 
